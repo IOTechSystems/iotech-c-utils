837eeb75bd03a87a8558a8f3a8428c9d348877cb
Steve Osselton <steve@iotechsys.com>
2018-08-22 17:31:57 +0100

    SDK-28 Update not to build deabian 8 form arm64

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -18,8 +18,12 @@ build_dist ubuntu-18.04
 if [ "$QUICK" != "true" ]
 then
   build_dist ubuntu-16.04
-  build_dist debian-9
-  build_dist debian-8
   build_dist alpine-3.7
   build_dist alpine-3.8
+  build_dist debian-9
+
+  if [ "$ARCH" != "arm64" ]
+  then
+    build_dist debian-8
+  fi
 fi
