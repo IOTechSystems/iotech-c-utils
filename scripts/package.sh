66b3ace49ff53c2f6b9c41ca72c6bc86c1f58a42
Tom Fleming <tom@iotechsys.com>
2018-09-20 11:13:38 +0100

    SDK-37 updated to use fixed apk builder version
    Signed-off-by: Tom Fleming <tom@iotechsys.com>

--- scripts/package.sh
+++ scripts/package.sh
@@ -11,9 +11,9 @@ build_apk()
   cp build/${DIST}/release/cutils-${VER}.tar.gz apk/${DIST}/.
   cp scripts/APKBUILD apk/${DIST}/.
   cp VERSION apk/${DIST}/.
-  docker pull docker.iotechsys.com/services/iotech-apk-builder-${ARCH}:${VER}
-  docker run --rm -e UID=`id -u ${USER}` -e GID=`id -g ${USER}` -v "$(pwd)"/apk/${DIST}:/home/packager/build docker.iotechsys.com/services/iotech-apk-builder-${ARCH}:${VER}
-  docker rmi docker.iotechsys.com/services/iotech-apk-builder-${ARCH}:${VER}
+  docker pull docker.iotechsys.com/services/iotech-apk-builder-${ARCH}:0.2.0
+  docker run --rm -e UID=`id -u ${USER}` -e GID=`id -g ${USER}` -v "$(pwd)"/apk/${DIST}:/home/packager/build docker.iotechsys.com/services/iotech-apk-builder-${ARCH}:0.2.0
+  docker rmi docker.iotechsys.com/services/iotech-apk-builder-${ARCH}:0.2.0
 }
 
 build_apk alpine-3.8
