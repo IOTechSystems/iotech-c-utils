605e43ab7f53af610a833522d5b9bf14166adff0
Steve Osselton <steve@iotechsys.com>
2019-02-26 16:58:49 +0000

    XM-8 Fix to pass board to docker image

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -4,13 +4,14 @@ set -x -e
 SYSTEM=$1
 ARCH=$2
 RAND=$3
+TARGET=$4
 export VER=`cat VERSION`
 
 build_dist()
 {
   mkdir -p ${ARCH}/build/${SYSTEM}
   docker build --tag iotech-c-utils-${SYSTEM}-${RAND}:${VER} --file scripts/Dockerfile.${SYSTEM} .
-  docker run --rm -v "$(pwd)"/${ARCH}/${SYSTEM}:/iotech-c-utils/${ARCH} iotech-c-utils-${SYSTEM}-${RAND}:${VER}
+  docker run --rm -e "TARGET=${TARGET}" -v "$(pwd)"/${ARCH}/${SYSTEM}:/iotech-c-utils/${ARCH} iotech-c-utils-${SYSTEM}-${RAND}:${VER}
   docker rmi iotech-c-utils-${SYSTEM}-${RAND}:${VER}
 }
 
