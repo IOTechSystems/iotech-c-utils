2ed9411336b2efb76cd80259ec99ad5bd650c88b
Steve Osselton <steve@iotechsys.com>
2019-04-18 15:29:30 +0100

    XM-37 threadpool unit test update

--- src/c/utests/threadpool/threadpool.c
+++ src/c/utests/threadpool/threadpool.c
@@ -50,6 +50,7 @@ static void cunit_threadpool_priority (void)
   iot_threadpool_add_work (pool, cunit_pool_worker, &prio2, &prio2);
   iot_threadpool_add_work (pool, cunit_pool_worker, &prio3, &prio3);
   iot_threadpool_wait (pool);
+  CU_ASSERT (iot_threadpool_num_threads_working (pool) == 0u);
   iot_threadpool_free (pool);
 }
 
