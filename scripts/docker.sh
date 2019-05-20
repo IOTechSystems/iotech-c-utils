cc543ebdd048d22a320fb6dc284940a0405f104a
Steve Osselton <steve@iotechsys.com>
2019-05-20 13:09:37 +0100

    XM-7 Script fix

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -9,10 +9,11 @@ export VER=`cat VERSION`
 
 build_dist()
 {
-  mkdir -p ${ARCH}/build/${SYSTEM}
-  docker build --pull --tag iotech-c-utils-${SYSTEM}-${RAND}:${VER} --file scripts/Dockerfile.${SYSTEM} .
-  docker run --cap-add=SYS_NICE --rm -e "TARGET=${TARGET}" -v "$(pwd)"/${ARCH}/${SYSTEM}:/iotech-c-utils/${ARCH} iotech-c-utils-${SYSTEM}-${RAND}:${VER}
-  docker rmi iotech-c-utils-${SYSTEM}-${RAND}:${VER}
+  SYS=$1
+  mkdir -p ${ARCH}/build/${SYS}
+  docker build --pull --tag iotech-c-utils-${SYS}-${RAND}:${VER} --file scripts/Dockerfile.${SYS} .
+  docker run --cap-add=SYS_NICE --rm -e "TARGET=${TARGET}" -v "$(pwd)"/${ARCH}/${SYS}:/iotech-c-utils/${ARCH} iotech-c-utils-${SYS}-${RAND}:${VER}
+  docker rmi iotech-c-utils-${SYS}-${RAND}:${VER}
 }
 
 build_dist $SYSTEM
