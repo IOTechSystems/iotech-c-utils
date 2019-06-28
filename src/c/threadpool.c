d4400a8ad33e1dc25a5b06819e1e6c31e4f75935
Steve Osselton <steve@iotechsys.com>
2019-06-28 13:28:32 +0100

    XM-62 More logging added

--- src/c/threadpool.c
+++ src/c/threadpool.c
@@ -228,10 +228,12 @@ bool iot_threadpool_try_work (iot_threadpool_t * pool, void (*func) (void*), voi
 
 void iot_threadpool_add_work (iot_threadpool_t * pool, void (*func) (void*), void * arg, const int * prio)
 {
+  iot_log_trace (pool->logger, "iot_threadpool_add_work()");
   assert (pool && func);
   pthread_mutex_lock (&pool->mutex);
   if (pool->jobs == pool->max_jobs)
   {
+    iot_log_debug (pool->logger, "iot_threadpool_add_work jobs at max (%u), waiting for job completion", pool->max_jobs);
     pthread_cond_wait (&pool->queue_cond, &pool->mutex); // Wait until space in job queue
   }
   iot_threadpool_add_job_locked (pool, func, arg, prio);
