34fe5029129b2081004d8516a541b6f13196d6fa
br <bindu@iotechsys.com>
2019-07-12 13:35:00 +0100

    XM-68: Add logger to Scheduler example

--- src/c/examples/scheduler.c
+++ src/c/examples/scheduler.c
@@ -4,6 +4,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 #include "iot/scheduler.h"
+#include "iot/logger.h"
 
 static void testFunc1 (void *in)
 {
@@ -22,53 +23,64 @@ static void testFunc3 (void *in)
 
 int main (void)
 {
+  iot_logger_t *logger = NULL;
+  logger = iot_logger_alloc ("Scheduler_Example", IOT_LOG_INFO);
+
+  iot_logger_start (logger);
+
   /* Create a threadpool */
-  printf ("Create the threadpool\n");
-  iot_threadpool_t * pool = iot_threadpool_alloc (4, 0, NULL, NULL);
+  iot_log_info (logger, "Create the threadpool");
+  iot_threadpool_t * pool = iot_threadpool_alloc (4, 0, NULL, logger);
 
   /* Initialise the scheduler */
-  printf ("Initialise the scheduler\n");
-  iot_scheduler_t * scheduler = iot_scheduler_alloc (pool, NULL);
+  iot_log_info (logger, "Initialise the scheduler");
+  iot_scheduler_t * scheduler = iot_scheduler_alloc (pool, logger);
+
 
   iot_threadpool_start (pool);
   iot_scheduler_start (scheduler);
 
   /* Create two schedules */
-  printf ("Create two schedules\n");
+  iot_log_info (logger, "Create two schedules");
   iot_schedule_t * sched1 = iot_schedule_create (scheduler, testFunc1, NULL, IOT_MS_TO_NS (500), 0, 0, NULL);
   iot_schedule_t * sched2 = iot_schedule_create (scheduler, testFunc2, NULL, IOT_SEC_TO_NS (1), 0, 0, NULL);
 
   /* Add two schedules to the scheduler */
-  printf ("Add two schedules\n");
+  iot_log_info (logger,"Add two schedules");
   iot_schedule_add (scheduler, sched1);
   iot_schedule_add (scheduler, sched2);
 
   /* Start the scheduler */
-  printf ("Start the scheduler\n");
+  iot_log_info (logger, "Start the scheduler");
   iot_scheduler_start (scheduler);
   sleep (5);
 
   /* Create and add a third schedule */
-  printf ("\nCreate and add schedule 3\n");
+  printf ("\n");
+  iot_log_info (logger, "Create and add schedule 3");
   iot_schedule_t * sched3 = iot_schedule_create (scheduler, testFunc3, NULL, IOT_SEC_TO_NS (2), 0, 2, NULL);
   iot_schedule_add (scheduler, sched3);
   sleep (5);
 
   /* Remove a schedule */
-  printf ("\nRemove schedule 1\n");
+  printf ("\n");
+  iot_log_info (logger, "Remove schedule 1");
   iot_schedule_remove (scheduler, sched1);
   sleep (5);
+
   /* Delete a schedule */
-  printf ("\nDelete all schedules\n");
+  printf ("\n");
+  iot_log_info (logger, "Delete all schedules");
   iot_schedule_delete (scheduler, sched2);
 
   /* Stop and delete the scheduler (and associated schedules) */
-  printf ("Stop and delete the scheduler\n");
+  iot_log_info (logger, "Stop and delete the scheduler");
   iot_scheduler_stop (scheduler);
   iot_scheduler_free (scheduler);
 
   /* Destroy the thread pool */
-  printf ("Destroy the thread pool\n");
+  iot_log_info (logger, "Destroy the thread pool");
   iot_threadpool_free (pool);
+  iot_logger_free (logger);
   return 0;
 }
