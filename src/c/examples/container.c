6db52a123283f6a6e28bcbd69fe82c2c882c93f6
Steve Osselton <steve@iotechsys.com>
2019-07-19 14:51:28 +0100

    XM-73 Container example cleanup

--- src/c/examples/container.c
+++ src/c/examples/container.c
@@ -19,58 +19,14 @@ typedef struct my_component_t
   iot_logger_t * logger;
 } my_component_t;
 
-extern bool my_component_start (my_component_t * comp)
-{
-  printf ("MyComponent started\n");
-  iot_component_set_running (&comp->component);
-  return true;
-}
+/* Basic exported component functions */
 
-extern void my_component_stop (my_component_t * comp)
-{
-  printf ("MyComponent stopped\n");
-  iot_component_set_stopped (&comp->component);
-}
-
-extern void my_component_add_ref (my_component_t * comp)
-{
-  iot_component_add_ref (&comp->component);
-}
-
-extern my_component_t * my_component_alloc (iot_logger_t * logger)
-{
-  printf ("MyComponent alloc\n");
-  my_component_t * mycomp = calloc (1, sizeof (*mycomp));
-  iot_component_init (&mycomp->component, (iot_component_start_fn_t) my_component_start, (iot_component_stop_fn_t) my_component_stop);
-  mycomp->logger = logger;
-  iot_logger_add_ref (logger);
-  return mycomp;
-}
-
-extern void my_component_free (my_component_t * comp)
-{
-  if (comp && iot_component_dec_ref (&comp->component))
-  {
-    printf ("MyComponent free\n");
-    iot_logger_free (comp->logger);
-    iot_component_fini (&comp->component);
-    free (comp);
-  }
-}
-
-static iot_component_t * my_component_config (iot_container_t * cont, const iot_data_t * map)
-{
-  printf ("MyComponent config\n");
-  const char * name = iot_data_string_map_get_string (map, "MyLogger");
-  iot_logger_t * logger = (iot_logger_t*) iot_container_find (cont, name);
-  return (iot_component_t*) my_component_alloc (logger);
-}
-
-static const iot_component_factory_t * my_component_factory (void)
-{
-  static iot_component_factory_t factory = { MY_COMPONENT_TYPE, my_component_config, (iot_component_free_fn_t) my_component_free };
-  return &factory;
-}
+extern my_component_t * my_component_alloc (iot_logger_t * logger);
+extern void my_component_add_ref (my_component_t * comp);
+extern void my_component_free (my_component_t * comp);
+extern bool my_component_start (my_component_t * comp);
+extern void my_component_stop (my_component_t * comp);
+extern const iot_component_factory_t * my_component_factory (void);
 
 int main (void)
 {
@@ -87,6 +43,59 @@ int main (void)
   iot_container_free (container);
   iot_fini ();
   return 0;
+}
+
+extern my_component_t * my_component_alloc (iot_logger_t * logger)
+{
+  printf ("MyComponent alloc\n");
+  my_component_t * mycomp = calloc (1, sizeof (*mycomp));
+  iot_component_init (&mycomp->component, (iot_component_start_fn_t) my_component_start, (iot_component_stop_fn_t) my_component_stop);
+  mycomp->logger = logger;
+  iot_logger_add_ref (logger);
+  return mycomp;
+}
+
+extern void my_component_add_ref (my_component_t * comp)
+{
+  iot_component_add_ref (&comp->component);
+}
+
+extern void my_component_free (my_component_t * comp)
+{
+  if (comp && iot_component_dec_ref (&comp->component))
+  {
+    printf ("MyComponent free\n");
+    iot_logger_free (comp->logger);
+    iot_component_fini (&comp->component);
+    free (comp);
+  }
+}
+
+extern bool my_component_start (my_component_t * comp)
+{
+  printf ("MyComponent started\n");
+  iot_component_set_running (&comp->component);
+  return true;
+}
+
+extern void my_component_stop (my_component_t * comp)
+{
+  printf ("MyComponent stopped\n");
+  iot_component_set_stopped (&comp->component);
+}
+
+static iot_component_t * my_component_config (iot_container_t * cont, const iot_data_t * map)
+{
+  printf ("MyComponent config\n");
+  const char * name = iot_data_string_map_get_string (map, "MyLogger");
+  iot_logger_t * logger = (iot_logger_t*) iot_container_find (cont, name);
+  return (iot_component_t*) my_component_alloc (logger);
+}
+
+extern const iot_component_factory_t * my_component_factory (void)
+{
+  static iot_component_factory_t factory = { MY_COMPONENT_TYPE, my_component_config, (iot_component_free_fn_t) my_component_free };
+  return &factory;
 }
 
 static const char * main_config =
