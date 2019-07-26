14c4a738b782d40f70f143198ee47eba93f93274
Steve Osselton <steve@iotechsys.com>
2019-07-26 09:44:45 +0100

    XM-75 Hash function added

---
+++ include/iot/hash.h
@@ -0,0 +1,20 @@
+//
+// Copyright (c) 2019 IOTech
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+#ifndef _IOT_HASH_H_
+#define _IOT_HASH_H_
+
+#include "iot/os.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern uint32_t iot_hash (const char * str);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ src/c/hash.c
@@ -0,0 +1,13 @@
+#include "iot/hash.h"
+
+/* Version 2 of the Bernstein djb2 hash function. */
+
+uint32_t iot_hash (const char * str)
+{
+  uint32_t hash = 538u;
+  while (*str)
+  {
+    hash = ((hash << 5u) + hash) ^ (uint8_t) *str++;
+  }
+  return hash;
+}
