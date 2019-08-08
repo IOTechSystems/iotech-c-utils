529b758268f6604b606a9067f52cf019bcb08234
Steve Osselton <steve@iotechsys.com>
2019-08-08 16:26:28 +0100

    XM-18 Build/code tidy

--- src/c/iot.c
+++ src/c/iot.c
@@ -3,12 +3,12 @@
 extern void iot_data_init (void);
 extern void iot_data_fini (void);
 
-extern void iot_init (void)
+void iot_init (void)
 {
   iot_data_init ();
 }
 
-extern void iot_fini (void)
+void iot_fini (void)
 {
   iot_data_fini ();
 }
