f9d39cffd5748ae63938627daa8acbcb5bd20320
Steve Osselton <steve@iotechsys.com>
2019-08-08 09:25:43 +0100

    XM-18 Factor out example component into it's own file

--- src/c/examples/container.c
+++ src/c/examples/container.c
@@ -3,30 +3,11 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 //
-#include "iot/iot.h"
+#include "mycomponent.h"
 
 /* Configuration loader function */
 
 static char * config_loader (const char * name, void * from);
-
-/* Boilerplate code for a component */
-
-#define MY_COMPONENT_TYPE "MyComponent"
-
-typedef struct my_component_t
-{
-  iot_component_t component;
-  iot_logger_t * logger;
-} my_component_t;
-
-/* Basic exported component functions */
-
-extern my_component_t * my_component_alloc (iot_logger_t * logger);
-extern void my_component_add_ref (my_component_t * comp);
-extern void my_component_free (my_component_t * comp);
-extern bool my_component_start (my_component_t * comp);
-extern void my_component_stop (my_component_t * comp);
-extern const iot_component_factory_t * my_component_factory (void);
 
 int main (void)
 {
@@ -70,61 +51,6 @@ int main (void)
   return 0;
 }
 
-/* Custom component boilerplate function implementations */
-
-extern my_component_t * my_component_alloc (iot_logger_t * logger)
-{
-  printf ("MyComponent alloc\n");
-  my_component_t * mycomp = calloc (1, sizeof (*mycomp));
-  iot_component_init (&mycomp->component, my_component_factory (), (iot_component_start_fn_t) my_component_start, (iot_component_stop_fn_t) my_component_stop);
-  mycomp->logger = logger;
-  iot_logger_add_ref (logger);
-  return mycomp;
-}
-
-extern void my_component_add_ref (my_component_t * comp)
-{
-  iot_component_add_ref (&comp->component);
-}
-
-extern void my_component_free (my_component_t * comp)
-{
-  if (comp && iot_component_dec_ref (&comp->component))
-  {
-    printf ("MyComponent free\n");
-    iot_logger_free (comp->logger);
-    iot_component_fini (&comp->component);
-    free (comp);
-  }
-}
-
-extern bool my_component_start (my_component_t * comp)
-{
-  printf ("MyComponent started\n");
-  iot_component_set_running (&comp->component);
-  return true;
-}
-
-extern void my_component_stop (my_component_t * comp)
-{
-  printf ("MyComponent stopped\n");
-  iot_component_set_stopped (&comp->component);
-}
-
-static iot_component_t * my_component_config (iot_container_t * cont, const iot_data_t * map)
-{
-  printf ("MyComponent config\n");
-  const char * name = iot_data_string_map_get_string (map, "MyLogger");
-  iot_logger_t * logger = (iot_logger_t*) iot_container_find (cont, name);
-  return (iot_component_t*) my_component_alloc (logger);
-}
-
-extern const iot_component_factory_t * my_component_factory (void)
-{
-  static iot_component_factory_t factory = { MY_COMPONENT_TYPE, my_component_config, (iot_component_free_fn_t) my_component_free, NULL };
-  return &factory;
-}
-
 /* Example component JSON configurations */
 
 static const char * main_config =
@@ -133,7 +59,7 @@ static const char * main_config =
   "\"logger\":\"IOT::Logger\","
   "\"pool\":\"IOT::ThreadPool\","
   "\"scheduler\":\"IOT::Scheduler\","
-  "\"mycomp\":\"MyComponent\""
+  "\"mycomp\":\"IOT::MyComponent\""
 "}";
 
 static const char * file_logger_config =
---
+++ src/c/examples/mycomponent.c
@@ -0,0 +1,63 @@
+#include "mycomponent.h"
+
+struct my_component_t
+{
+  iot_component_t component;
+  iot_logger_t * logger;
+};
+
+/* Custom component boilerplate function implementations */
+
+extern my_component_t * my_component_alloc (iot_logger_t * logger)
+{
+  printf ("MyComponent alloc\n");
+  my_component_t * mycomp = calloc (1, sizeof (*mycomp));
+  iot_component_init (&mycomp->component, my_component_factory (), (iot_component_start_fn_t) my_component_start, (iot_component_stop_fn_t) my_component_stop);
+  mycomp->logger = logger;
+  iot_logger_add_ref (logger);
+  return mycomp;
+}
+
+extern void my_component_add_ref (my_component_t * comp)
+{
+  iot_component_add_ref (&comp->component);
+}
+
+extern void my_component_free (my_component_t * comp)
+{
+  if (comp && iot_component_dec_ref (&comp->component))
+  {
+    printf ("MyComponent free\n");
+    iot_logger_free (comp->logger);
+    iot_component_fini (&comp->component);
+    free (comp);
+  }
+}
+
+extern bool my_component_start (my_component_t * comp)
+{
+  printf ("MyComponent started\n");
+  iot_component_set_running (&comp->component);
+  return true;
+}
+
+extern void my_component_stop (my_component_t * comp)
+{
+  printf ("MyComponent stopped\n");
+  iot_component_set_stopped (&comp->component);
+}
+
+static iot_component_t * my_component_config (iot_container_t * cont, const iot_data_t * map)
+{
+  printf ("MyComponent config\n");
+  const char * name = iot_data_string_map_get_string (map, "MyLogger");
+  iot_logger_t * logger = (iot_logger_t*) iot_container_find (cont, name);
+  return (iot_component_t*) my_component_alloc (logger);
+}
+
+extern const iot_component_factory_t * my_component_factory (void)
+{
+  static iot_component_factory_t factory = { MY_COMPONENT_TYPE, my_component_config, (iot_component_free_fn_t) my_component_free, NULL };
+  return &factory;
+}
+
