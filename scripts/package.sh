fc4e6502ad531bc0ad32a62f60ef8575761a8682
Steve Osselton <steve@iotechsys.com>
2019-10-30 12:41:04 +0000

    XM-7 APK generation tidy

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,40 +2,32 @@
 set -x -e
 SYSTEM=$1
 ARCH=$2
-export VER=$(cat VERSION)
+APKARCH=$3
+VER=$(cat VERSION)
 BUILDER=iotechsys/iotech-apk-builder:0.2.0
-APKARCH=x86_64
-
-case ${ARCH} in
-  arm64)
-    APKARCH=aarch64
-  ;;
-  arm32)
-    APKARCH=armv7
-  ;;
-esac
+ARCHIVE="${ARCH}/${DIST}/release/iotech-iot-${VER}_${APKARCH}.tar.gz"
 
 build_apk()
 {
   DIST=$1
-  mkdir -p apk/${DIST}
-  cp ${ARCH}/${DIST}/release/iotech-iot-*.tar.gz apk/${DIST}/
-  cp scripts/APKBUILD apk/${DIST}/.
-  cp VERSION apk/${DIST}/.
-  docker run --rm -e UID=$(id -u ${USER}) -e GID=$(id -g ${USER}) -v "$(pwd)/apk/${DIST}:/home/packager/build" ${BUILDER}
+  mkdir -p "apk/${DIST}"
+  cp "${ARCHIVE}" "apk/${DIST}/"
+  cp scripts/APKBUILD "apk/${DIST}/."
+  cp VERSION "apk/${DIST}/."
+  docker run --rm -e UID=$(id -u ${USER}) -e GID=$(id -g ${USER}) -v "$(pwd)/apk/${DIST}:/home/packager/build" "${BUILDER}"
 }
 
 build_dbg_apk()
 {
   DIST=$1
-  rm apk/${DIST}/packager/${APKARCH}/APKINDEX.tar.gz
-  cp ${ARCH}/${DIST}/debug/iotech-iot-*.tar.gz apk/${DIST}/
-  sed -e's/pkgname=iotech-iot/&-dbg/' <scripts/APKBUILD >apk/${DIST}/APKBUILD
-  cp VERSION apk/${DIST}/.
-  docker run --rm -e UID=$(id -u ${USER}) -e GID=$(id -g ${USER}) -v "$(pwd)/apk/${DIST}:/home/packager/build" ${BUILDER}
+  rm "apk/${DIST}/packager/${APKARCH}/APKINDEX.tar.gz"
+  cp "${ARCHIVE}" "apk/${DIST}/"
+  sed -e's/pkgname=iotech-iot/&-dbg/' <scripts/APKBUILD >"apk/${DIST}/APKBUILD"
+  cp VERSION "apk/${DIST}/."
+  docker run --rm -e UID=$(id -u ${USER}) -e GID=$(id -g ${USER}) -v "$(pwd)/apk/${DIST}:/home/packager/build" "${BUILDER}"
 }
 
-docker pull ${BUILDER}
-build_apk ${SYSTEM}
-build_dbg_apk ${SYSTEM}
+docker pull "${BUILDER}"
+build_apk "${SYSTEM}"
+build_dbg_apk "${SYSTEM}"
 
