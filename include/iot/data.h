cc03b8dd2d8119c29531742e4653bb236016f40a
br <bindu@iotechsys.com>
2020-01-16 15:51:44 +0000

    XM-27: Document update

--- include/iot/data.h
+++ include/iot/data.h
@@ -459,7 +459,7 @@ extern const uint8_t * iot_data_blob (const iot_data_t * data, uint32_t * size);
  * @param map  Map to add a new key-value pair
  * @param key  Input key
  * @param val  Input value
- * Note: The ownership of key and value supplied to a map is owned by the map and cannot be reused, unless reference counted
+ * Note: The ownership of key and value passed is owned by the map and cannot be reused, unless reference counted
  */
 extern void iot_data_map_add (iot_data_t * map, iot_data_t * key, iot_data_t * val);
 
@@ -468,7 +468,7 @@ extern void iot_data_map_add (iot_data_t * map, iot_data_t * key, iot_data_t * v
  *
  * The function to get the map size
  *
- * @param map Map to get the size
+ * @param map Input map
  * @return    Size of the map
  */
 extern uint32_t iot_data_map_size (const iot_data_t * map);
@@ -481,7 +481,7 @@ extern uint32_t iot_data_map_size (const iot_data_t * map);
  * @param map  Map to add a new key-value pair
  * @param key  Input key of string type
  * @param val  Input value
- * Note: The ownership of key and value supplied to a map is owned by the map and cannot be reused, unless reference counted
+ * Note: The ownership of key and value passed is owned by the map and cannot be reused, unless reference counted
  */
 extern void iot_data_string_map_add (iot_data_t * map, const char * key, iot_data_t * val);
 
@@ -571,7 +571,7 @@ extern bool iot_data_map_base64_to_blob (iot_data_t * map, const iot_data_t * ke
  * @param array  Input array to add an element
  * @param index  Index in an array
  * @param val    Pointer to a value of type iot_data to add
- * Note: The ownership of the value supplied to array is owned by the array and cannot be reused, unless reference counted
+ * Note: The ownership of the value passed is owned by the array and cannot be reused, unless reference counted
  */
 extern void iot_data_array_add (iot_data_t * array, uint32_t index, iot_data_t * val);
 
