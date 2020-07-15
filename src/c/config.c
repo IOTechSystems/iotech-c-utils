c5567dfe30b0732c2d28eef49e8780e0021a88e6
Steve Osselton <steve@iotechsys.com>
2020-07-15 17:38:07 +0100

    XRT-335 Assert fix

--- src/c/config.c
+++ src/c/config.c
@@ -59,7 +59,7 @@ const char * iot_config_string (const iot_data_t * map, const char * key, bool a
 
 extern const char * iot_config_string_default (const iot_data_t * map, const char * key, const char * def, bool alloc)
 {
-  assert (map && key && def);
+  assert (map && key);
   const char * val = iot_data_string_map_get_string (map, key);
   if (val == NULL) val = def;
   if (alloc && val) val = strdup (val);
