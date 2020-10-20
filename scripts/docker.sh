16056c74b5643b9baa9347fbbad10b3d593d9955
Steve Osselton <steve@iotechsys.com>
2020-10-20 13:34:42 +0100

    XRT-380 Shell script CLion warning removal

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -5,17 +5,17 @@ SYS=$1
 ARCH=$2
 TARGET=$3
 RAND=$$
-export VER=$(cat VERSION)
+VER=$(cat VERSION)
 
 if [ "$ARCH" = "x86" ]
 then
   DEXT="i386-"
 fi
 
-mkdir -p ${ARCH}/build/${SYS}
-docker build --pull --no-cache --tag iotech-iot-${SYS}-${RAND}:${VER} --file scripts/Dockerfile.${DEXT}${SYS} .
-docker run --cap-add=SYS_NICE --rm -e "TARGET=${TARGET}" -e "USER=${USER}" -v "$(pwd)/${ARCH}/${SYS}:/iotech-iot/${ARCH}" iotech-iot-${SYS}-${RAND}:${VER}
+mkdir -p "${ARCH}/build/${SYS}"
+docker build --pull --no-cache --tag "iotech-iot-${SYS}-${RAND}:${VER}" --file "scripts/Dockerfile.${DEXT}${SYS}" .
+docker run --cap-add=SYS_NICE --rm -e "TARGET=${TARGET}" -e "USER=${USER}" -v "$(pwd)/${ARCH}/${SYS}:/iotech-iot/${ARCH}" "iotech-iot-${SYS}-${RAND}:${VER}"
 RET=$?
-docker rmi iotech-iot-${SYS}-${RAND}:${VER}
+docker rmi "iotech-iot-${SYS}-${RAND}:${VER}"
 
 exit ${RET}
