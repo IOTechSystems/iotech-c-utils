d816e596da4d247aff0ba6fc38e5784c8011afe1
Edward Scott <31443281+edward-scott@users.noreply.github.com>
2021-08-24 17:52:22 +0100

    XRT-717: Change iot_scheduler_start start parameter to use relative time (#169)

--- include/iot/scheduler.h
+++ include/iot/scheduler.h
@@ -103,7 +103,7 @@ extern void iot_scheduler_start (iot_scheduler_t * scheduler);
  * @param  free_func          The function to free the arg parameter when scheduler is deleted, could be NULL
  * @param  arg                The argument to be passed to the function
  * @param  period             The period of the schedule (in nanoseconds)
- * @param  start              The start time of the schedule (in nanoseconds)
+ * @param  start              The time to wait until the schedule is first triggered (in nanoseconds)
  * @param  repeat             The number of times the schedule should repeat, (0 = infinite)
  * @param  pool               The thread pool used to run the schedule
  * @param  priority           The thread priority for running the schedule, (not set if -1)
