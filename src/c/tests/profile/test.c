79a7899ca0aaf6aaa8c78156bd6659962e2bf16d
Steve Osselton <steve@iotechsys.com>
2021-10-15 09:55:28 +0100

    XRT-940 parse cache test added

---
+++ src/c/tests/profile/test.c
@@ -0,0 +1,28 @@
+#include "iot/iot.h"
+
+#ifndef NDEBUG
+extern void iot_data_map_dump (iot_data_t * map);
+#else
+#define iot_data_map_dump(n)
+#endif
+
+int main (int argc, char ** argv)
+{
+  if (argc != 2)
+  {
+    fprintf (stderr, "Usage: test <profile file>\n");
+    exit (-1);
+  }
+  iot_init ();
+  char * profile = iot_file_read (argv[1]);
+  if (profile == NULL)
+  {
+    fprintf (stderr, "Failed to read file: %s\n", argv[1]);
+    exit (-1);
+  }
+  iot_data_t * cache = iot_data_alloc_map (IOT_DATA_STRING);
+  iot_data_t * map = iot_data_from_json_with_cache (profile, false, cache);
+ // iot_data_map_dump (cache);
+  iot_fini ();
+  return 0;
+}
