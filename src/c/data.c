079554fe7948c6f6d55e7f9aafef14bdddd71490
Steve Osselton <steve@iotechsys.com>
2021-10-28 14:09:27 +0100

    XRT-940 Map reverse iterator added

--- src/c/data.c
+++ src/c/data.c
@@ -188,7 +188,9 @@ extern void iot_data_map_dump (iot_data_t * map);
 static iot_data_t * iot_data_value_from_json (iot_json_tok_t ** tokens, const char * json, bool ordered, iot_data_t * cache);
 static void iot_node_free (iot_node_t * node);
 static iot_node_t * iot_node_start (iot_node_t * node);
+static iot_node_t * iot_node_end (iot_node_t * node);
 static iot_node_t * iot_node_next (iot_node_t * iter);
+static iot_node_t * iot_node_prev (iot_node_t * iter);
 static bool iot_node_add (iot_data_map_t * map, iot_data_t * key, iot_data_t * value);
 static bool iot_node_remove (iot_data_map_t * map, const iot_data_t * key);
 static iot_node_t * iot_node_find (const iot_node_t * node, const iot_data_t * key);
@@ -1076,6 +1078,13 @@ bool iot_data_map_iter_next (iot_data_map_iter_t * iter)
 {
   assert (iter);
   iter->node = (iter->node) ? iot_node_next (iter->node) : iot_node_start (iter->map->tree);
+  return (iter->node != NULL);
+}
+
+bool iot_data_map_iter_prev (iot_data_map_iter_t * iter)
+{
+  assert (iter);
+  iter->node = (iter->node) ? iot_node_prev (iter->node) : iot_node_end (iter->map->tree);
   return (iter->node != NULL);
 }
 
@@ -2289,6 +2298,13 @@ static iot_node_t * iot_node_start (iot_node_t * node)
   return start;
 }
 
+static iot_node_t * iot_node_end (iot_node_t * node)
+{
+  iot_node_t * end = node;
+  if (end) while (end->right) end = end->right;
+  return end;
+}
+
 static iot_node_t * iot_node_next (iot_node_t * iter)
 {
   if (iter->right)
@@ -2301,6 +2317,27 @@ static iot_node_t * iot_node_next (iot_node_t * iter)
     // While the right child, chain up parent link
     iot_node_t * n = iter->parent;
     while (n && iter == n->right)
+    {
+      iter = n;
+      n = n->parent;
+    }
+    iter = n;
+  }
+  return iter;
+}
+
+static iot_node_t * iot_node_prev (iot_node_t * iter)
+{
+  if (iter->left)
+  {
+    // Left then right to the end
+    for (iter = iter->left; iter->right != NULL; iter = iter->right);
+  }
+  else
+  {
+    // While the left child, chain up parent link
+    iot_node_t * n = iter->parent;
+    while (n && iter == n->left)
     {
       iter = n;
       n = n->parent;
