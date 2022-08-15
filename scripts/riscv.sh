500fe73e72f43f26bf209520cbe3a15ebce51eed
Steve Osselton <steve@iotechsys.com>
2022-08-15 11:38:46 +0100

    XRT-1729 Update to use BOARD for RISC-V build target

--- scripts/riscv.sh
+++ scripts/riscv.sh
@@ -8,16 +8,29 @@ PKG_VER=$(cut -d . -f 1,2 < ${ROOT}/VERSION)
 OS_ARCH=`dpkg --print-architecture`
 SYS_NAME=Linux
 
-. /opt/oecore-riscv64/environment-setup-riscv64-oe-linux
+case "${TARGET}" in
+  riscv64-oe)
+    . /opt/oecore-riscv64/environment-setup-riscv64-oe-linux
+    RISCV=riscv64
+  ;;
+  riscv32-oe)
+    . /opt/oecore-riscv32/environment-setup-riscv32-oe-linux
+    RISCV=riscv32
+  ;;
+  *)
+    echo "RISC-V Target BOARD not set or invalid"
+    exit 1
+  ;;
+esac
 
 mkdir -p "${BROOT}/release"
 cd "${BROOT}/release"
 cmake \
-  -DCMAKE_TOOLCHAIN_FILE=/opt/oecore-riscv64/sysroots/x86_64-oesdk-linux/usr/share/cmake/riscv64-oe-linux-toolchain.cmake \
+  -DCMAKE_TOOLCHAIN_FILE=/opt/oecore-${RISCV}/sysroots/x86_64-oesdk-linux/usr/share/cmake/${TARGET}-linux-toolchain.cmake \
   -DCMAKE_BUILD_TYPE="MinSizeRel" \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
   -DLINUX_SYS=RISCV64 \
-  -DSYSROOT=/opt/oecore-riscv64/sysroots/riscv64-oe-linux \
+  -DSYSROOT=/opt/oecore-${RISCV}/sysroots/${TARGET}-linux \
   -DIOT_BUILD_COMPONENTS=ON \
   -DIOT_BUILD_DYNAMIC_LOAD=ON \
   "${ROOT}/src"
@@ -27,11 +40,11 @@ make package
 mkdir -p "${BROOT}/debug"
 cd "${BROOT}/debug"
 cmake \
-  -DCMAKE_TOOLCHAIN_FILE=/opt/oecore-riscv64/sysroots/x86_64-oesdk-linux/usr/share/cmake/riscv64-oe-linux-toolchain.cmake \
+  -DCMAKE_TOOLCHAIN_FILE=/opt/oecore-${RISCV}/sysroots/x86_64-oesdk-linux/usr/share/cmake/${TARGET}-linux-toolchain.cmake \
   -DCMAKE_BUILD_TYPE="Debug" \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
   -DLINUX_SYS=RISCV64 \
-  -DSYSROOT=/opt/oecore-riscv64/sysroots/riscv64-oe-linux \
+  -DSYSROOT=/opt/oecore-${RISCV}/sysroots/${TARGET}-linux \
   -DIOT_BUILD_COMPONENTS=ON \
   -DIOT_BUILD_DYNAMIC_LOAD=ON \
   "${ROOT}/src"
@@ -43,7 +56,7 @@ fpm -s dir -t deb -n iotech-iot-riscv-${PKG_VER}-dev -v "${VER}" \
   --chdir _CPack_Packages/${SYS_NAME}/TGZ/iotech-iot-${PKG_VER}-${VER}_${OS_ARCH} \
   --deb-no-default-config-files --deb-priority "optional" --category "devel" \
   --prefix opt/iotech/iot/riscv/${PKG_VER} \
-  --description "IOT C Framework (RISC-V)" \
+  --description "IOT C Framework (${TARGET})" \
   --vendor "IOTech" --maintainer "IOTech Support <support@iotechsys.com>" \
   --depends cmake --depends make
 rm *.tar.gz
@@ -53,7 +66,7 @@ fpm -s dir -t deb -n iotech-iot-riscv-${PKG_VER}-dbg -v "${VER}" \
   --chdir _CPack_Packages/${SYS_NAME}/TGZ/iotech-iot-dev-${PKG_VER}-${VER}_${OS_ARCH} \
   --deb-no-default-config-files --deb-priority "optional" --category "devel" \
   --prefix opt/iotech/iot/riscv/${PKG_VER} \
-  --description "IOT C Framework (RISC-V)" \
+  --description "IOT C Framework (${TARGET})" \
   --vendor "IOTech" --maintainer "IOTech Support <support@iotechsys.com>" \
   --depends cmake --depends make
 rm *.tar.gz
