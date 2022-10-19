b77684ac9726e67981895853d54d5fd4189e0e5b
Joe Riemersma <joepaz200@gmail.com>
2022-10-19 13:00:45 +0100

    XRT-1887: fix iot_data_is_nan

--- src/c/data.c
+++ src/c/data.c
@@ -1057,7 +1057,7 @@ bool iot_data_is_nan (const iot_data_t *data)
   assert (data);
   iot_data_type_t type = iot_data_type (data);
   if (type != IOT_DATA_FLOAT32 && type != IOT_DATA_FLOAT64) return false;
-  return IOT_DATA_FLOAT32 ? isnanf (iot_data_f32 (data)) != 0 : isnan (iot_data_f64 (data)) != 0;
+  return type == IOT_DATA_FLOAT32 ? isnanf (iot_data_f32 (data)) != 0 : isnan (iot_data_f64 (data)) != 0;
 }
 
 static void iot_data_cache_add (iot_data_t * cache, iot_data_t ** data)
