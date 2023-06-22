e8e2e9fe310ba6bacbcfad49a07577f3ea7ef212
Steve Osselton <steve@iotechsys.com>
2023-06-22 15:28:48 +0200

    XRT-2372 Script tidy

--- scripts/add_deb_repo.sh
+++ scripts/add_deb_repo.sh
@@ -2,19 +2,13 @@
 set -x -e
 
 SYS=$(lsb_release -c -s)
-REL=$1
-
-if [ "${REL}" = "dev" ]
-then
-  cp /iotech-iot/scripts/auth.conf /etc/apt/auth.conf
-fi
 
 if [ "${SYS}" = "jammy" ]
 then
   curl -fsSL https://iotech.jfrog.io/artifactory/api/gpg/key/public | gpg --dearmor -o /usr/share/keyrings/iotech.gpg
-  echo "deb [signed-by=/usr/share/keyrings/iotech.gpg] https://iotech.jfrog.io/iotech/debian-${REL} ${SYS} main" >> /etc/apt/sources.list.d/iotech.list
+  echo "deb [signed-by=/usr/share/keyrings/iotech.gpg] https://iotech.jfrog.io/iotech/debian-release ${SYS} main" >> /etc/apt/sources.list.d/iotech.list
 else
   curl -fsSL https://iotech.jfrog.io/artifactory/api/gpg/key/public | apt-key add -
-  echo "deb https://iotech.jfrog.io/iotech/debian-${REL} ${SYS} main" >> /etc/apt/sources.list.d/iotech.list
+  echo "deb https://iotech.jfrog.io/iotech/debian-release ${SYS} main" >> /etc/apt/sources.list.d/iotech.list
 fi
 apt-get update
