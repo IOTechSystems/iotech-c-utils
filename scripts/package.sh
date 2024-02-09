bc4eb8d3efa625d3cc070f28ff034d217ea4d15a
Steve Osselton <steve@iotechsys.com>
2024-02-09 15:07:38 +0000

    XRT-2743 Alpine 3.19 Support (#328)
    * XRT-2743 Initial support added for Alpine 3.19
    
    * XRT-2743 Update to add package release version
    
    * XRT-2743 Version handling fix
    
    * XRT-2743 Version handling fix #2
    
    * XRT-2743 Update to add package release version #2
    
    * XRT-2743 Update to add package release version #3
    
    * XRT-2743 Update to add package release version #4
    
    * XRT-2743 Release version update
    
    * XRT-2743 Apline package release ver fix
    
    * XRT-2743 FPM full version update

--- scripts/package.sh
+++ scripts/package.sh
@@ -25,8 +25,10 @@ do
 done
 
 BROOT="${ROOT}/${BARCH}"
-VER=$(cat ${ROOT}/VERSION)
+VER=$(cut -d . -f 1,2,3 < ${ROOT}/VERSION)
+FULL_VER=$(cut -d . -f 1,2,3,4 < ${ROOT}/VERSION)
 PKG_VER=$(cut -d . -f 1,2 < ${ROOT}/VERSION)
+REL_VER=$(cut -d . -f 4 < ${ROOT}/VERSION)
 
 MAINT_EMAIL="IOTech Support <support@iotechsys.com>"
 DESC_MAIN="IOT C Framework"
@@ -64,7 +66,7 @@ case ${SYSTEM} in
         ;;
     esac
 
-    export VER PKG_VER OS_ARCH
+    export VER PKG_VER REL_VER OS_ARCH
     mkdir /iotech-iot/${BARCH}/apks
     chmod 0644 /iotech-iot/scripts/apk.key
     printf '%s' "PACKAGER_PRIVKEY=/iotech-iot/scripts/apk.key" >> /etc/abuild.conf
@@ -82,7 +84,7 @@ case ${SYSTEM} in
     OS_ARCH=$(dpkg --print-architecture)
     cd ${ROOT}/${BARCH}/release
 
-    ${FPM} -s dir -t deb -n iotech-iot-${PKG_VER} -v "${VER}" \
+    ${FPM} -s dir -t deb -n iotech-iot-${PKG_VER} -v "${FULL_VER}" \
       --deb-no-default-config-files --deb-changelog ../../RELEASE_NOTES.md \
       -C _CPack_Packages/Linux/TGZ/iotech-iot-${PKG_VER}-${VER}_${OS_ARCH} \
       --deb-priority "optional" --category "devel" --prefix /opt/iotech/iot/${PKG_VER} \
@@ -91,7 +93,7 @@ case ${SYSTEM} in
       --exclude include --exclude docs --exclude examples \
       --depends libyaml-0-2
 
-    ${FPM} -s dir -t deb -n iotech-iot-${PKG_VER}-dev -v "${VER}" \
+    ${FPM} -s dir -t deb -n iotech-iot-${PKG_VER}-dev -v "${FULL_VER}" \
       --deb-no-default-config-files --deb-changelog ../../RELEASE_NOTES.md \
       -C _CPack_Packages/Linux/TGZ/iotech-iot-${PKG_VER}-${VER}_${OS_ARCH} \
       --deb-priority "optional" --category "devel" --prefix /opt/iotech/iot/${PKG_VER} \
@@ -104,7 +106,7 @@ case ${SYSTEM} in
 
     cd ${ROOT}/${BARCH}/debug
 
-    ${FPM} -s dir -t deb -n iotech-iot-${PKG_VER}-dbg -v "${VER}" \
+    ${FPM} -s dir -t deb -n iotech-iot-${PKG_VER}-dbg -v "${FULL_VER}" \
       --deb-no-default-config-files --deb-changelog ../../RELEASE_NOTES.md \
       -C _CPack_Packages/Linux/TGZ/iotech-iot-dev-${PKG_VER}-${VER}_${OS_ARCH} \
       --deb-priority "optional" --category "devel" --prefix /opt/iotech/iot/${PKG_VER} \
@@ -147,7 +149,7 @@ case ${SYSTEM} in
 
     cd ${ROOT}/${BARCH}/release
 
-    ${FPM} -s dir -t rpm -n iotech-iot-${PKG_VER} -v "${VER}" \
+    ${FPM} -s dir -t rpm -n iotech-iot-${PKG_VER} -v "${FULL_VER}" \
       -C _CPack_Packages/Linux/TGZ/iotech-iot-${PKG_VER}-${VER}_${OS_ARCH} \
       --architecture "${OS_ARCH}" ${RPM_DIST:+--rpm-dist ${RPM_DIST}} \
       --prefix /opt/iotech/iot/${PKG_VER} \
@@ -156,7 +158,7 @@ case ${SYSTEM} in
       --exclude include --exclude docs --exclude examples \
       --depends libyaml
 
-    ${FPM} -s dir -t rpm -n iotech-iot-${PKG_VER}-dev -v "${VER}" \
+    ${FPM} -s dir -t rpm -n iotech-iot-${PKG_VER}-dev -v "${FULL_VER}" \
       -C _CPack_Packages/Linux/TGZ/iotech-iot-${PKG_VER}-${VER}_${OS_ARCH} \
       --architecture "${OS_ARCH}" ${RPM_DIST:+--rpm-dist ${RPM_DIST}} \
       --prefix /opt/iotech/iot/${PKG_VER} \
@@ -169,7 +171,7 @@ case ${SYSTEM} in
 
     cd ${ROOT}/${BARCH}/debug
 
-    ${FPM} -s dir -t rpm -n iotech-iot-${PKG_VER}-dbg -v "${VER}" \
+    ${FPM} -s dir -t rpm -n iotech-iot-${PKG_VER}-dbg -v "${FULL_VER}" \
       -C _CPack_Packages/Linux/TGZ/iotech-iot-dev-${PKG_VER}-${VER}_${OS_ARCH} \
       --architecture "${OS_ARCH}" ${RPM_DIST:+--rpm-dist ${RPM_DIST}} \
       --prefix /opt/iotech/iot/${PKG_VER} \
