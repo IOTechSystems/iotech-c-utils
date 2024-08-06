3c3ad5cd7edee1ede23e4e6bc47f692c994b1189
Steve Osselton <steve@iotechsys.com>
2024-08-06 10:36:40 +0100

    XRT-3011 (#348)
    * XRT-3011 Alpine 3.20 support added
    
    * XRT-3011 Build fix

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -9,9 +9,19 @@ BRANCH=$5
 RAND=$$
 VER=$(cat VERSION)
 
+# Build fix for https://github.com/docker-library/official-images/issues/16829
+
+if [ "${ARCH}" = "arm32" ] || [ "${ARCH}" = "x86" ]
+then
+  if [ "${SYS}" = "alpine-3.20" ]
+  then
+    DOCKER_RUN_FLAGS="--security-opt seccomp=unconfined"
+  fi
+fi
+
 mkdir -p "${ARCH}/build/${SYS}"
 docker build --pull --no-cache --tag "iotech-iot-${SYS}-${RAND}:${VER}" --file "scripts/Dockerfile.${SYS}" .
-docker run --cap-add=SYS_NICE --rm -e "BRANCH=${BRANCH}" -e "TARGET=${TARGET}" -e "USER=${USER}" -e "SONAR=${SONAR}" -e "SONAR_AUTH_TOKEN=${SONAR_AUTH_TOKEN}" -v "$(pwd)/${ARCH}/${SYS}:/iotech-iot/${ARCH}" "iotech-iot-${SYS}-${RAND}:${VER}"
+docker run ${DOCKER_RUN_FLAGS} --cap-add=SYS_NICE --rm -e "BRANCH=${BRANCH}" -e "TARGET=${TARGET}" -e "USER=${USER}" -e "SONAR=${SONAR}" -e "SONAR_AUTH_TOKEN=${SONAR_AUTH_TOKEN}" -v "$(pwd)/${ARCH}/${SYS}:/iotech-iot/${ARCH}" "iotech-iot-${SYS}-${RAND}:${VER}"
 RET=$?
 docker rmi "iotech-iot-${SYS}-${RAND}:${VER}"
 
